6276d3da0d91ac453d73e55ec2782c521f7da742
PROX-E <lichlord18@gmail.com>
2020-05-12 19:42:38 -0600

    more post instance changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,17 +3,17 @@ import axios from "axios";
 export default {
   // Gets all posts
   getPosts: function() {
-    return axios.get("/api/books");
+    return axios.get("/api/posts");
   },
-  // Gets the book with the given id
+  // Gets the post with the given id
   getPost: function(id) {
-    return axios.get("/api/books/" + id);
+    return axios.get("/api/posts/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the post with the given id
   deletePost: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/posts/" + id);
   },
-  // Saves a book to the database
+  // Saves a post to the database
   savePost: function(postData) {
     return axios.post("/api/posts", postData);
   }
