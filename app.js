caaa9d80a1dd1708f95fe43255bafa34a5e118a3
Bikashbhaila <bikash.bhaila@outlook.com>
2020-05-23 14:10:53 -0500

    add heroku components

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 app.use(cookieParser());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/mernauth', {useNewUrlParser : true, useUnifiedTopology: true },()=>{
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  const UserRouter = require('./routes/User');
+  app.use('/user', UserRouter);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mernauth', {useNewUrlParser : true, useUnifiedTopology: true },()=>{
     console.log('successfully connected to database');
 });
 
-const UserRouter = require('./routes/User');
-app.use('/user', UserRouter);
+// const UserRouter = require('./routes/User');
+// app.use('/user', UserRouter);
 
-app.listen(5000,()=>{
-    console.log('express server started');
-});
+// app.listen(5000,()=>{
+//     console.log('express server started');
+// });
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
