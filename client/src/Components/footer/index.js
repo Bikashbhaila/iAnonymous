3e719f0d7952f002bd34346b85350b531ca04c4a
PROX-E <lichlord18@gmail.com>
2020-05-29 06:52:42 -0600

    icons and footer styling

---
+++ client/src/Components/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>UofM Coding Bootcamp 2020</p>
+            <div className="container Logo">
+
+			</div> 
+
+		</div>	
+	</footer>
+);
+
+export default Footer;
